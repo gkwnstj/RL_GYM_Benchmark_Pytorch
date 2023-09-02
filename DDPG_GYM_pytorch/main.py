b27141ad92eb1f5de112c6ce3802a8402b35c126
gkwnstj <87974851+gkwnstj@users.noreply.github.com>
2023-09-02 19:00:32 +0900

    Add files via upload

---
+++ DDPG_GYM_pytorch/main.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+from DDPG import * 
+from reporter import *
+
+def main():
+
+
+    max_episode_num = 1000  # 최대 에피소드 설정
+    env = gym.make("Pendulum-v1", g=9.81)
+    agent = DDPGagent(env)  # DDPG 에이전트 객체
+
+    # 학습 진행
+    agent.train(max_episode_num)
+
+    # 학습 결과 도시
+    agent.plot_result()
+
+
+if __name__=="__main__":
+    main()
